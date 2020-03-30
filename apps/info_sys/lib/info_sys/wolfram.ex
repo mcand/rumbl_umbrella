696053e37ba94f585ba67663f5e412280be4fa88
Andre Furquim <andreccomputacao@gmail.com>
2020-03-29 21:03:35 -0300

    Test Infosys and mock backend

--- apps/info_sys/lib/info_sys/wolfram.ex
+++ apps/info_sys/lib/info_sys/wolfram.ex
@@ -25,16 +25,16 @@ defmodule InfoSys.Wolfram do
     [%Result{backend: __MODULE__, score: 95, text: to_string(answer)}]
   end
 
+  @http Application.get_env(:info_sys, :wolfram)[:http_client] || :httpc
   defp fetch_xml(query) do
-    {:ok, {_, _, body}} = :httpc.request(String.to_charlist(url(query)))
+    {:ok, {_, _, body}} = @http.request(String.to_charlist(url(query)))
 
     body
   end
 
   defp url(input) do
-    "#{@base}?" <>
-    URI.encode_query(appid: id(), input: input, format: "plaintext")
+    "#{@base}?" <> URI.encode_query(appid: id(), input: input, format: "plaintext")
   end
 
-  defp id, do: Application.fetch_env!(:info_sys, :wolfram)[:app_id]
+  defp id, do: Application.get_env(:info_sys, :wolfram)[:app_id]
 end
--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,9 @@
 use Mix.Config
 
+config :info_sys, :wolfram,
+  app_id: "1234",
+  http_client: InfoSys.Test.HTTPClient
+
 # Configure your database
 config :rumbl, Rumbl.Repo,
   username: "postgres",
